a40144195b5d2926235ceb9b172b0899c2291735
Phil Allcock <phil.allcock@dubitlimited.com>
2016-07-28 11:11:48 +0100

    Added single finger drag rotation reset fix from https://github.com/sscaff1/react-native-gestures

--- src/drag.js
+++ src/drag.js
@@ -4,7 +4,7 @@ import oneFingerResponder from './responder/oneFinger'
 export let responder = oneFingerResponder
 
 export let transducer = map(function (gesture) {
-  let layout = gesture.get('initialLayout').set('rotate', 0)
+  let layout = gesture.get('initialLayout')
   let initialTouch = gesture.get('initialTouches').get(0)
   let currentTouch = gesture.get('touches').get(0)
 
@@ -14,5 +14,6 @@ export let transducer = map(function (gesture) {
         (currentTouch.get('pageX') - initialTouch.get('pageX')))
       .set('y', l.get('y') +
         (currentTouch.get('pageY') - initialTouch.get('pageY')))
+      .set('rotate', l.get('rotate'))
   }).toJS()
 })
